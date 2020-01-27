79094be11aa6762f3b4e764cedae39a36cd012e3
Andreas Linnert <andreas@linnertmedia.de>
2020-01-27 23:09:13 +0100

    Ensure that also necessary files do exist

--- src/setupEnv.ts
+++ src/setupEnv.ts
@@ -1,16 +1,32 @@
 import * as fs from 'fs'
 
+const baseDir = `${__dirname}/..`
+
 const requiredDirectories = [
-  'storage/sessions',
+  'storage/sessions'
+]
+
+const requiredFiles = [
+  'storage/data.json'
 ]
 
 function ensureDirectories () {
   for (const directory of requiredDirectories) {
-    const mkdirOptions = { recursive: true, mode: 0o755 }
-    fs.mkdirSync(`${__dirname}/../${directory}`, mkdirOptions)
+    const options: fs.MakeDirectoryOptions = { recursive: true, mode: 0o755 }
+    const path = `${baseDir}/${directory}`
+    fs.mkdirSync(path, options)
+  }
+}
+
+function ensureFiles () {
+  for (const file of requiredFiles) {
+    const options: fs.WriteFileOptions = { mode: 0o755, flag: 'wx' }
+    const path = `${baseDir}/${file}`
+    try { fs.writeFileSync(path, '', options) } catch {}
   }
 }
 
 export function setupEnvironment () {
   ensureDirectories()
+  ensureFiles()
 }
