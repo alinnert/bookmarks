23e96c08e94db7b759a025e630957c304dc60112
Andreas Linnert <andreas@linnertmedia.de>
2020-02-10 19:50:21 +0100

    Implement authentication redirects
    If logged in: '/login' → '/'
    If not logged in '*' → '/login'

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,34 +1,58 @@
 import React, { FC, useEffect, useState } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, useLocation, useHistory } from 'react-router-dom'
 import { Navigation } from './components/Navigation'
-import { loadCurrentUser } from './modules/auth'
+import { loadCurrentUser, useCurrentUser } from './modules/auth'
 import { Home } from './views/Home'
 import { Login } from './views/Login'
 
 export const App: FC = () => {
+  useAuthRedirects()
+  const { error, ready } = useInit()
+
+  const errorContent = <div>Oops! An error occured while loading the app.</div>
+  const loadingContent = <div>Loading...</div>
+  const appContent = <>
+    <Navigation/>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={Login} />
+    </Switch>
+  </>
+
+  return (
+    <div className="App">
+      {ready ? appContent : error ? errorContent : loadingContent}
+    </div>
+  )
+}
+
+function useInit (): { ready: boolean, error: boolean } {
   const [ready, setReady] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
 
   useEffect(() => {
     loadCurrentUser()
       .then(() => { setReady(true) })
-      .catch((error) => { console.error(error) })
+      .catch((error) => {
+        setError(true)
+        console.error(error)
+      })
   }, [])
 
-  return (
-    <div className="App">
-      {ready ? (
-        <>
-          <Navigation/>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/login" exact component={Login} />
-          </Switch>
-        </>
-      ) : (
-        <>
-          <div>App wird geladen...</div>
-        </>
-      )}
-    </div>
-  )
+  return { ready, error }
+}
+
+function useAuthRedirects (): void {
+  const location = useLocation()
+  const history = useHistory()
+  const { currentUser } = useCurrentUser()
+
+  if (location.pathname === '/login' && currentUser !== null) {
+    history.replace('/')
+    return
+  }
+
+  if (location.pathname !== '/login' && currentUser === null) {
+    history.replace('/login')
+  }
 }
