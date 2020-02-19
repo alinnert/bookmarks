74b94bb1e6e0f6206de486ebea5d1932ee39f492
Andreas Linnert <andreas@linnertmedia.de>
2020-02-19 20:20:26 +0100

    Fix auth redirect behaviour
    Redirects shouldn't happen if the user hasn't been loaded yet.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,24 +6,24 @@ import { Home } from './views/Home'
 import { Login } from './views/Login'
 
 export const App: FC = () => {
-  useAuthRedirects()
   const { error, ready } = useInit()
-
-  const errorContent = <div>Oops! An error occured while loading the app.</div>
-
-  const loadingContent = <div>Loading...</div>
-
-  const appContent = <>
-    <Navigation/>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/login" exact component={Login} />
-    </Switch>
-  </>
+  useAuthRedirects(ready)
 
   return (
     <div className="App">
-      {ready ? appContent : error ? errorContent : loadingContent}
+      {ready ? (
+        <>
+          <Navigation/>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </>
+      ) : error ? (
+        <div>Oops! An error occured while loading the app.</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   )
 }
@@ -44,12 +44,14 @@ function useInit (): { ready: boolean, error: boolean } {
   return { ready, error }
 }
 
-function useAuthRedirects (): void {
+function useAuthRedirects (ready: boolean): void {
   const location = useLocation()
   const history = useHistory()
   const { currentUser } = useCurrentUser()
 
   useEffect((): void => {
+    if (!ready) { return }
+
     if (location.pathname === '/login' && currentUser !== null) {
       history.replace('/')
       return
